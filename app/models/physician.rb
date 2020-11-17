fab721de89b2e54c9ebc7b7e555d30327b3331bb
andy <jia470079834@gmail.com>
2020-11-16 22:37:21 -0500

    fix auth

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -23,7 +23,7 @@ class Physician < ApplicationRecord
     has_many :appointments
     has_many :patients, through: :appointments
 
-    has_one_attached :photo
+    has_one_attached :phcoto
 
     def password=(password)
         @password = password
--- app/views/api/physicians/_physician.json.jbuilder
+++ app/views/api/physicians/_physician.json.jbuilder
@@ -1,5 +1,5 @@
 json.extract! physician, :id, :physician_name, :email, :npi
 
-if physician.photo.attached?
-    json.photoUrl url_for(physician.photo)
-end
+# if physician.photo.attached?
+#     json.photoUrl url_for(physician.photo)
+# end

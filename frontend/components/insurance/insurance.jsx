c8570fde6ff8177d3250fd1a18ea34c2fca2d51e
andy <jia470079834@gmail.com>
2020-12-07 23:32:18 -0500

    add insurance

--- frontend/components/insurance/insurance.jsx
+++ frontend/components/insurance/insurance.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Insurance extends React.Component {
-    constructor(){
-        
+    constructor(props){
+        super(props);
     }
 }
 
